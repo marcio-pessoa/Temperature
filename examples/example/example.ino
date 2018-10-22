1660ddc7b3e655983147199a8a3e60fee7d8eca1
Márcio Pessoa <30762851+marcio-pessoa@users.noreply.github.com>
2018-10-22 13:51:02 -0300

    Added some comments

--- examples/example/example.ino
+++ examples/example/example.ino
@@ -1,19 +1,20 @@
 #include <Temperature.h>
 
-Temperature sensor_a;
-Temperature sensor_b;
+Temperature sensor_a;  // Create sensor object
+Temperature sensor_b;  // Create sensor object
 
 void setup() {
   Serial.begin(9600);
-  sensor_a.attach(0);
-  sensor_a.type(LM35);
-  sensor_b.attach(1);
-  sensor_b.type(TMP36);
+  sensor_a.attach(0);  // Analog input 0
+  sensor_a.type(LM35);  // Set sensor type as LM35
+  sensor_b.attach(1);  // Analog input 1
+  sensor_b.type(TMP36);  // Set sensor type as TMP36
 }
 
 void loop() {
+  // Display results
   Serial.println(String(sensor_a.read()) + " celsius");
   Serial.println(String(sensor_b.read()) + " celsius");
   Serial.println();
-  delay(2000);
+  delay(2000);  // Wait 2 seconds
 }
